ccff94f7246c9182f155309002e9028e4a79a179
VWRoli <fustroli@gmail.com>
2022-04-10 10:01:37 +0200

    chore: set up react-native-paper

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import * as React from 'react';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
-AppRegistry.registerComponent(appName, () => App);
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#1976d2',
+  },
+};
+
+export default function Main() {
+  return (
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
