b53e7fb6122385e82146f85bfcf1020525aff323
VWRoli <fustroli@gmail.com>
2022-04-10 11:13:04 +0200

    feat: add drawer

--- App.tsx
+++ App.tsx
@@ -1,18 +1,35 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { View } from 'react-native';
-import { Button } from 'react-native-paper';
+import { View, Text } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function NotificationsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Notification Screen</Text>
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Button
-        icon="camera"
-        mode="contained"
-        onPress={() => console.log('Pressed')}
-      >
-        Press me
-      </Button>
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 };
 
--- index.js
+++ index.js
@@ -1,23 +1,5 @@
-import * as React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: '#1976d2',
-  },
-};
-
-export default function Main() {
-  return (
-    <PaperProvider theme={theme}>
-      <App />
-    </PaperProvider>
-  );
-}
-
-AppRegistry.registerComponent(appName, () => Main);
+AppRegistry.registerComponent(appName, () => App);
