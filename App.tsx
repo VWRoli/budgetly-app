4a2bad81da4737c7b10bdd8d23184d17cee3f657
VWRoli <fustroli@gmail.com>
2022-04-10 11:28:27 +0200

    feat: implement navigation

--- App.tsx
+++ App.tsx
@@ -1,33 +1,27 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { View, Text } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { LogBox } from 'react-native';
+//Screens
+import Budget from './screens/Budget';
+import Reports from './screens/Reports';
+import Transactions from './screens/Transactions';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-function NotificationsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Notification Screen</Text>
-    </View>
-  );
-}
+//To ignore warning
+LogBox.ignoreLogs([
+  "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+]);
 
 const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      <Drawer.Navigator initialRouteName="Budget">
+        <Drawer.Screen name="Budget" component={Budget} />
+        <Drawer.Screen name="Reports" component={Reports} />
+        <Drawer.Screen name="Transactions" component={Transactions} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
---
+++ screens/Budget.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Budget = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Budget Screen</Text>
+    </View>
+  );
+};
+
+export default Budget;
