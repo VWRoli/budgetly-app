fa6d93684b6b74e1d0a9de8c222e5f3cd6facc26
VWRoli <fustroli@gmail.com>
2022-05-16 20:39:12 +0200

    feat: add progress bar

--- src/components/Budget/BudgetItem.tsx
+++ src/components/Budget/BudgetItem.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { budgetItemType } from '../../types/budgetItemType';
 import { formatter } from '../../utils/helpers';
+import * as Progress from 'react-native-progress';
 //Components
 import CustomText from '../common/CustomText';
 import Chip from '../common/Chip';
-import Date from '../common/Date';
 
 interface Props {
   item: budgetItemType;
@@ -27,7 +27,24 @@ const BudgetItem: React.FC<Props> = ({ item }): JSX.Element => {
 
         <Chip value={formatter(item.balance)} />
       </View>
-      <Date />
+      <Progress.Circle
+        //todo progress data not clear yet
+        progress={0.7}
+        color="#06B3C4"
+        unfilledColor="#eee"
+        borderWidth={0}
+        size={60}
+        strokeCap="round"
+        thickness={6}
+        showsText
+        //shows 0 if animated is on https://github.com/oblador/react-native-progress/issues/241
+        animated={false}
+        formatText={(progress) => (
+          <Text style={{ fontSize: 16, fontWeight: 'bold' }}>
+            {progress * 100}%
+          </Text>
+        )}
+      />
     </View>
   );
 };
