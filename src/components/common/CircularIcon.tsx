d453135d7342aa45802ac0fe80cf028eeeea9e2f
VWRoli <fustroli@gmail.com>
2022-05-18 19:28:55 +0200

    feat: transaction fab

--- src/components/common/CircularIcon.tsx
+++ src/components/common/CircularIcon.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { View } from 'react-native';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const CircularIcon = () => {
+interface Props {
+  children: React.ReactNode;
+  size?: number;
+  bgColor?: string;
+}
+const CircularIcon: React.FC<Props> = ({
+  children,
+  size = 35,
+  bgColor = '#06B3C4',
+}): JSX.Element => {
   return (
     <View
       style={{
-        backgroundColor: '#06B3C4',
-        height: 35,
-        width: 35,
-        borderRadius: 35 / 2,
+        backgroundColor: bgColor,
+        height: size,
+        width: size,
+        borderRadius: size / 2,
         justifyContent: 'center',
         alignItems: 'center',
       }}
     >
-      <MaterialCommunityIcons
-        name="file-document-multiple-outline"
-        size={20}
-        color="#fff"
-      />
+      {children}
     </View>
   );
 };
--- src/screens/TransactionsScreen.tsx
+++ src/screens/TransactionsScreen.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-//Components
-import { ScrollView, Text, View } from 'react-native';
-import TransactionCard from '../components/TransactionCard';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Dimensions,
+} from 'react-native';
 import { BASE_URL } from '../constants/constants';
 import { useFetch } from '../hooks/useFetch';
 import { transactionType } from '../types/transactionType';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+//Components
+import TransactionCard from '../components/TransactionCard';
+import CircularIcon from '../components/common/CircularIcon';
 
 const TransactionsScreen = () => {
   const {
@@ -14,35 +23,55 @@ const TransactionsScreen = () => {
   } = useFetch(`${BASE_URL}users/1/transactions`);
 
   return (
-    <ScrollView
-      contentContainerStyle={{
-        alignItems: 'center',
-      }}
+    <View
       style={{
         flex: 1,
-        backgroundColor: '#fff',
       }}
     >
-      <View style={{ width: '85%' }}>
-        {isLoading ? (
-          <Text>Loading</Text>
-        ) : (
-          transactions.map((tr: transactionType) => (
-            <TransactionCard
-              payee={tr.payee}
-              amount={tr.amount}
-              date={tr.date}
-              category={
-                tr.income
-                  ? 'Income'
-                  : `${tr.categoryTitle}/${tr.budgetItemTitle}`
-              }
-            />
-          ))
-        )}
-      </View>
-    </ScrollView>
+      <ScrollView
+        contentContainerStyle={{
+          alignItems: 'center',
+        }}
+        style={{
+          flex: 1,
+          backgroundColor: '#f8f7f7',
+        }}
+      >
+        <View style={{ width: '85%' }}>
+          {isLoading ? (
+            <Text>Loading</Text>
+          ) : (
+            transactions.map((tr: transactionType) => (
+              <TransactionCard
+                key={tr.id}
+                payee={tr.payee}
+                amount={tr.amount}
+                date={tr.date}
+                category={
+                  tr.income
+                    ? 'Income'
+                    : `${tr.categoryTitle}/${tr.budgetItemTitle}`
+                }
+              />
+            ))
+          )}
+        </View>
+      </ScrollView>
+      <TouchableOpacity activeOpacity={0.7} style={styles.fab}>
+        <CircularIcon size={45} bgColor="#1eff05">
+          <Icon name="plus-thick" color="#fff" size={30} />
+        </CircularIcon>
+      </TouchableOpacity>
+    </View>
   );
 };
 
+const styles = StyleSheet.create({
+  fab: {
+    position: 'absolute',
+    bottom: 25,
+    left: Dimensions.get('screen').width / 2,
+    alignSelf: 'flex-end',
+  },
+});
 export default TransactionsScreen;
