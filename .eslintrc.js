123288963e32a3ac4be9717da95320f9fca0e4f2
VWRoli <fustroli@gmail.com>
2022-06-14 19:00:59 +0200

    chore: eslint precommit hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: '',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       rules: {
-        '@typescript-eslint/no-shadow': ['error'],
+        '@typescript-eslint/no-shadow': ['off'],
         'no-shadow': 'off',
         'no-undef': 'off',
         '@typescript-eslint/no-unused-vars': ['warn'],
