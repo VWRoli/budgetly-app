3f9922b399b3a38ca51b505a9bcc0ae8f1eb16a9
VWRoli <fustroli@gmail.com>
2022-06-18 15:15:38 +0200

    feat: reg success screen

--- src/screens/RegistrationSuccessScreen.tsx
+++ src/screens/RegistrationSuccessScreen.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Logo from '../assets/Logo';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+//Componenets
+import HeaderText from '../components/common/HeaderText';
+import Button from '../components/common/Button';
 
-const RegistrationSuccessScreen = () => {
+const RegistrationSuccessScreen = ({ navigation }: { navigation: any }) => {
   return (
-    <View>
-      <Text>RegistrationSuccessScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 35,
+        backgroundColor: '#fff',
+      }}
+    >
+      <Logo />
+      <View style={{ alignItems: 'center' }}>
+        <HeaderText
+          text="Registration Successful!"
+          styles={{ color: '#00c851', marginBottom: 10 }}
+          size={26}
+        />
+        <Icon name="check-circle-outline" color="#00c851" size={46} />
+      </View>
+      <Button
+        label="Enter budgetly"
+        pressHandler={() => navigation.navigate('Budget')}
+      />
     </View>
   );
 };
