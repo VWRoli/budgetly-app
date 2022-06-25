54ad1b5f2083862119b1f5c4942f802f819993c6
VWRoli <fustroli@gmail.com>
2022-06-25 15:27:04 +0200

    feat: login validation and loading state

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { ActivityIndicator, View } from 'react-native';
 import Logo from '../assets/Logo';
+import { useFormik } from 'formik';
+import { useAuthContext } from '../context/AuthContext';
+import { LoginSchema } from '../constants/LoginSchema';
 //Components
 import Input from '../components/common/Input';
 import Button from '../components/common/Button';
@@ -10,8 +13,25 @@ import HeaderText from '../components/common/HeaderText';
 import Container from '../components/common/Container';
 
 const LoginScreen = ({ navigation }: { navigation: any }) => {
-  const [text, setText] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const { signIn } = useAuthContext();
+  const [isLoading, setIsLoading] = useState(false);
+  const { handleChange, handleSubmit, errors, touched, handleBlur } = useFormik(
+    {
+      // validationSchema: LoginSchema,
+      initialValues: { username: '', email: '', password: '' },
+      onSubmit: async (values) => {
+        // setIsLoading(true);
+        // const res = await api.signUp(values);
+
+        // if (res) {
+        //   navigation.navigate('Success');
+        signIn();
+        // }
+
+        // setIsLoading(false);
+      },
+    },
+  );
 
   return (
     <Container>
@@ -26,16 +46,31 @@ const LoginScreen = ({ navigation }: { navigation: any }) => {
       </View>
       <Input
         placeholder="Email"
-        value={text}
-        changeHandler={() => setText(text)}
+        autoCapitalize="none"
+        autoCompleteType="email"
+        keyboardType="email-address"
+        changeHandler={handleChange('email')}
+        onBlur={handleBlur('email')}
+        error={errors.email}
+        touched={touched.email}
         icon="alternate-email"
+        editable={!isLoading}
+        returnKeyType="next"
+        returnKeyLabel="next"
       />
       <Input
         placeholder="Password"
-        value={text}
         secureTextEntry
         icon="lock-outline"
-        changeHandler={() => setPassword(password)}
+        changeHandler={handleChange('password')}
+        autoCompleteType="password"
+        autoCapitalize="none"
+        editable={!isLoading}
+        onBlur={handleBlur('password')}
+        error={errors.password}
+        touched={touched.password}
+        returnKeyType="go"
+        returnKeyLabel="go"
       />
 
       <View
@@ -46,20 +81,27 @@ const LoginScreen = ({ navigation }: { navigation: any }) => {
           paddingVertical: 15,
         }}
       >
-        <Link
-          text="Forgot password?"
-          pressHandler={() => navigation.navigate('Reminder')}
-        />
+        {isLoading ? (
+          <View style={{ flex: 1 }}>
+            <ActivityIndicator size="large" color="#06B3C4" />
+          </View>
+        ) : (
+          <Link
+            text="Forgot password?"
+            pressHandler={() => navigation.navigate('Reminder')}
+          />
+        )}
       </View>
-      <Button label="Login" pressHandler={() => {}} />
-
-      <View style={{ flexDirection: 'row', marginTop: 25 }}>
-        <CustomText text="New to budgetly?" styles={{ marginRight: 5 }} />
-        <Link
-          text="Register"
-          pressHandler={() => navigation.navigate('Signup')}
-        />
-      </View>
+      <Button label="Login" pressHandler={handleSubmit} disabled={isLoading} />
+      {isLoading || (
+        <View style={{ flexDirection: 'row', marginTop: 25 }}>
+          <CustomText text="New to budgetly?" styles={{ marginRight: 5 }} />
+          <Link
+            text="Register"
+            pressHandler={() => navigation.navigate('Signup')}
+          />
+        </View>
+      )}
     </Container>
   );
 };
