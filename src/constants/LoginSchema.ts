d991deae3f536fff55a02d89abfc3f91bb13ac9b
VWRoli <fustroli@gmail.com>
2022-07-02 11:01:31 +0200

    fix: login schema

--- src/constants/LoginSchema.ts
+++ src/constants/LoginSchema.ts
@@ -1,8 +1,9 @@
 import * as Yup from 'yup';
 export const LoginSchema = Yup.object().shape({
-  email: Yup.string()
-    .email('Please enter a valid email')
-    .required('Email is required'),
+  username: Yup.string()
+    .min(6, 'Username must be at least 6 character')
+    .max(16, 'Username is too long - maximum 16 characters allowed')
+    .required('Username is required'),
   password: Yup.string()
     .required('Password is required')
     .min(8, 'Password is too short - must be at least 8 characters long')
